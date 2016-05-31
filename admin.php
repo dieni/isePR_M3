a720c8a01cc36b217f8fd0abcb1ecaea89a9ae26
dieni <rockwithdc@gmail.com>
2016-05-31 03:10:22 +0200

    küchen anzeigen fertig

--- admin.php
+++ admin.php
@@ -13,7 +13,7 @@
     <header class="food-header">
         <div class="container-fluid">
             <div class="container">
-                <h1><i class="fa fa-cutlery"></i> Administration</h1>
+                <h1><i class="fa fa-bar-chart"></i> Statistik</h1>
             </div>
         </div>
     </header>
@@ -67,51 +67,86 @@
 		<h3>Welche Speißen werden wie oft angeboten</h3>
 		<?php
 		
-			/*Join:
-				caterer with catererskitchen over catererId
-				and then with kitchen over kitchenId
-			*/		
-			//Count how often each kitchen is offered by caterers  
 			$query = "SELECT kitchenDescription, catererId FROM kitchen INNER JOIN catererskitchen ON kitchen.kitchenId = catererskitchen.kitchenId";
 			$result = mysqli_query($connection, $query);
 			
 			
-			
+			//Count how often each kitchen is offered by caterers 
 			if(!$result){
 				echo "Es sind keine Caterer registriert";
 			} else {	
 				
+				//Fetch query in multidimensonal array
 				$i = 0;
 				$tableTemp = array();
-
 				while ($row = mysqli_fetch_assoc($result)){
 					$tableTemp[$i]['kitchenDescription'] = $row['kitchenDescription'];
 					$tableTemp[$i]['catererId'] = $row['catererId'];
 					$i++;
 				}
 				
+				//do the count thing
+				$i = 0;	
 				$table = array();
-				foreach($tableTemp as &$tempRow){
-					/* foreach($table as &$row){
-						if(row['kitchenDescription'] == tempRow['kitchenDescription']){
-							
-						}						
-					} */
-					array_push($table, 'kitchenDescription' => tempRow['kitchenDescription'], 'quantityVendor' => 1);
-					echo $tempRow['kitchenDescription']. " " .$tempRow['catererId']. "<br>";					
+				foreach($tableTemp as $tempRow){
+					$inserted = false;
+
+					//kitchenDescription already in table? if yes, count plus 1
+					foreach($table as &$row){
+						if($row['kitchenDescription'] == $tempRow['kitchenDescription']){
+							$row['quantityCaterer'] += 1;
+							$inserted = true;
+						}
+					}
+					
+					//If kitchenDescription was already in table, don't create a new entry
+					if(!$inserted){
+						$table[$i]['kitchenDescription'] = $tempRow['kitchenDescription'];
+						$table[$i]['quantityCaterer'] = 1;
+						$i++;
+					}
 				}
 				
+				//sort array
+				usort($table, function($a, $b) {
+					return $b['quantityCaterer'] - $a['quantityCaterer'];
+				});
+				
+				//Display result
+				$i = 0;
+				$arrayLength = sizeof($table);
+				while($i<$arrayLength){
+					echo $table[$i]['kitchenDescription']. ": " .$table[$i]['quantityCaterer']. "<br>";
+					$i++;
+				}
 				
 			}
 			
 			
-				
+		?>
+		
+		<h3>Welche durchschnittliche Bewertung haben die Caterer</h3>
+		
+		<?php
+		/* $query = "SELECT sterne FROM caterer INNER JOIN bewertungen";
+			$result = mysqli_query($connection, $query);
 			
+			if(!$result){
+				echo "Es sind keine Caterer registriert";
+			} else {
+				$averageRating = 0;
+				$countRatings = 0; //count how much caterer are registered
+				while($row = mysqli_fetch_assoc($result)){
+					$averageRating += $row['sterne'];
+					$countRatings++;
+				}
+				
+				echo round($averageRating/$countRatings,2);
+			} */
 		?>
 		
 		
-		
-		
+		<h3>Welche Zusatzpakete werden wie oft angeboten</h3>
 		
 		
 	</div>
@@ -125,7 +160,6 @@ STATISTIK:
 - Welche Speisen wie oft angeboten werden (türkisch, italienisch, etc.).
 - Was die durchschnittliche Bewertung der Caterer ergiebt (nur jene die auch wirklich bewertet wurden).
 - Welche Zusatzpakete wie oft gekauft wurden.
-- Was ist die durchschnittliche Bewertung der Caterer
 
 FUNKTION:
 - Caterer entfernen
